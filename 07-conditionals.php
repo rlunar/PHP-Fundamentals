cb75dc3482fb88db0b7451c23e321a25fff5e5e6
Roberto Luna Rojas <roberto.luna.rojas@gmail.com>
2014-01-05 18:26:41 -0500

    Loops - For, Foreach, While

--- 07-conditionals.php
+++ 07-conditionals.php
@@ -2,32 +2,48 @@
 $month = 'January';
 
 if ($month == 'January') {
-	echo 'It is January';
+    echo 'It is January';
 } else if ($month == 'February') {
-	echo 'It is February';
+    echo 'It is February';
 } else {
-	echo 'Not the right month!';
+    echo 'Not the right month!';
 }
 
 echo "<br>";
 
 switch ($month) {
-	case 'January':
-		echo 'It is Jan';
-		break;
-	case 'February':
-		echo 'It is Feb';
-		break;
-	default:
-		echo 'Not the right month!';
+    case 'January':
+        echo 'It is Jan';
+        break;
+    case 'February':
+        echo 'It is Feb';
+        break;
+    default:
+        echo 'Not the right month!';
 }
 
 echo "<br>";
 
 $months = array(
-	'January'	=>	'It is January',
-	'February'	=>	'It is February',
-	'March'		=>	'It is March'
+    'January'   =>  'It is January',
+    'February'  =>  'It is February',
+    'March'     =>  'It is March'
 );
 
-echo $months[$month];
+echo isset($months[$month]) ? $months[$month] : 'Not the right month!';
+
+echo "<br>";
+$month = 'May';
+
+if (($month !== 'May') && ($month !== 'June')) {
+    echo 'Is not May or June';
+} else {
+    echo "It is May or June";
+}
+
+echo "<br>";
+if (($month !== 'May') || ($month !== 'June')) {
+    echo "It is May or June";
+} else {
+    echo 'Is not May or June';
+}
